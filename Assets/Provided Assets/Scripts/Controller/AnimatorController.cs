dd1686052aa86dc67504f2962b926fa0d8131990
Faiq Laiq <faiqlaiq5@gmail.com>
2020-04-28 07:51:06 +0500

    Character Imported Animations, materials, textures applied Animator Controller

---
+++ Assets/Provided Assets/Scripts/Controller/AnimatorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TakeInput();
+    }
+
+    void TakeInput()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetInteger("isMoving", 1);
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetInteger("isMoving", -1);
+        }
+        else
+        {
+            animator.SetInteger("isMoving", 0);
+        }
+    }
+}
